cfbb54a04c37080455365581e2179746cad4e0d0
axatbhardwaj <axatbhardwaj@outlook.com>
2025-01-31 13:58:28 +0530

    added staking contracts

---
+++ packages/dvilela/contracts/service_staking_token/contract.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2023-2024 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This module contains the class to connect to the `ServiceStakingTokenMechUsage` contract."""
+
+from enum import Enum
+
+from aea.common import JSONLike
+from aea.configurations.base import PublicId
+from aea.contracts.base import Contract
+from aea.crypto.base import LedgerApi
+
+
+class ServiceStakingTokenContract(Contract):
+    """The Service Staking contract."""
+
+    contract_id = PublicId.from_str("valory/service_staking_token:0.1.0")
+
+    @classmethod
+    def build_checkpoint_tx(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+    ) -> JSONLike:
+        """Build checkpoint tx."""
+        contract_instance = cls.get_instance(ledger_api, contract_address)
+        data = contract_instance.encodeABI("checkpoint")
+        return dict(data=bytes.fromhex(data[2:]))
+
+
+    @classmethod
+    def get_next_checkpoint_ts(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+    ) -> JSONLike:
+        """Get the next checkpoint's timestamp."""
+        contract = cls.get_instance(ledger_api, contract_address)
+        ts = contract.functions.getNextRewardCheckpointTimestamp().call()
+        return dict(data=ts)
+
+    @classmethod
+    def ts_checkpoint(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+    ) -> JSONLike:
+        """Retrieve the checkpoint's timestamp."""
+        contract = cls.get_instance(ledger_api, contract_address)
+        ts_checkpoint = contract.functions.tsCheckpoint().call()
+        return dict(data=ts_checkpoint)
+
