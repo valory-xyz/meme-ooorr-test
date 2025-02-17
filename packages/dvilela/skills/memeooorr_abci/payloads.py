1f299b728b09e9a5189f4f43b43af4d2cb2a8402
axatbhardwaj <axatbhardwaj@outlook.com>
2025-02-18 04:03:58 +0530

    modified payload for EnaggeTwitter Behaviour

--- packages/dvilela/skills/memeooorr_abci/payloads.py
+++ packages/dvilela/skills/memeooorr_abci/payloads.py
@@ -58,6 +58,7 @@ class EngageTwitterPayload(BaseTxPayload):
     """Represent a transaction payload for the EngageTwitterRound."""
 
     event: str
+    mech_request: Optional[str]
 
 
 @dataclass(frozen=True)
@@ -113,13 +114,6 @@ class CallCheckpointPayload(BaseTxPayload):
 
     tx_submitter: str
     tx_hash: Optional[str]
-
-
-@dataclass(frozen=True)
-class PreMechRequestPayload(BaseTxPayload):
-    """Represent a transaction payload for the PreMechRequestRound."""
-
-    content: str
 
 
 @dataclass(frozen=True)
